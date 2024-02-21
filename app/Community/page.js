b4b15d2b9ee26fb275184eed5e238b61ea81658f
faizanali556 <faizanaliawan4444@gmail.com>
2024-02-21 15:46:45 +0500

    faizan commit

--- app/Community/page.js
+++ app/Community/page.js
@@ -1,45 +1,46 @@
-import teamImage from './Img/community.jpg';
+
 
 import React from 'react';
-import telegram from './Img/telegram.png'
+
 const page = () => {
-    console.log(teamImage)
+    
   return (
     <div id='community'>
-      <div className='relative sm:py-[96px] py-[64px] '>
+      <div className='relative  sm:py-[96px] py-[64px] '>
         <div className=" w-[91%] lg:max-w-[785px] mx-auto">
-      <h2  className='f-f-r-b font-medium text-xl md:text-4xl text-primary text-center'  >Community</h2>
+      <h2  className=' font-medium text-3xl md:text-4xl  text-center text-orange-500'  >Community</h2>
       <p className=" text-grey-dark f-f-m text-base text-center ">Join the fast growing PuppyLove Army.</p>
-      <div className='  grid grid-cols-12 gap-5  mt-[64px]  items-center'>
-        <div className=' col-span-12 md:col-span-6 hidden md:block   '>
-      <img src="/_next/static/media/community.18cfe37f.jpg" alt="community"  className="rounded-[35px] w-[365px] h-[350px] "/> </div>
-      {/* Other content */}
-      {/* .......................TELEGRAM.............. */}
-      <div className= 'col-span-12 md:col-span-6'>
-        <div className=' grid grid-cols-12 items-center gap-6  '>
-          <div className=" col-span-6 ">
-          <a href="https://t.me/PuppyLoveCoin">
-          <div className=" text-center w-full md:w-[162px] h-[162px] rounded-[24px] p-[32px] bg-blend-color-burn bg-primary-vdark ">
-            <img src={telegram} class=" w-[48px] h-[48px] mx-auto "/>
-            <h2 className=" f-f-sm text-white text-lg mt-5 ">Telegram</h2>
-            </div>
-          </a>
-          </div>
-          {/* ............................ */}
-          <div className=" col-span-6   ">
-            <a href="https://twitter.com/PuppyLoveCoin">
-            <div className=" text-center w-full md:w-[162px] h-[162px] rounded-[24px] p-[32px] bg-primary-vdark ">
-            <img src="/static/media/Twitter - Original.a71c1b2d1b5dc7c2c5b1812e0440c31d.svg" className=" w-[48px] h-[48px] mx-auto "/>
-            <h2 className=" f-f-sm text-white text-lg mt-5 ">Twitter</h2>
-          </div>
-          </a>
-          </div>
-        </div>
-      </div>
-      <div className=" col-span-12 md:col-span-6  md:hidden   ">
-        <img src="/_next/static/media/community.18cfe37f.jpg" className=" w-full h-[365px] object-cover rounded-[44px] "/>
-      </div>
-    </div>
+      <div className=" grid grid-cols-12 md:gap-4 
+ mt-[64px]  items-center">
+<div className=" col-span-12 md:col-span-6  hidden md:block">
+<img src="community.jpg"
+ className="rounded-3xl sm:w-[320px] sm:h-[300px] w-[365px] h-[350px]"/>
+
+ </div>
+<div className=" col-span-12 md:col-span-6 ">
+<div className=" grid grid-cols-12 items-center  md:gap-7 gap-6  ">
+<div className=" col-span-6  ">
+<a href="https://t.me/PuppyLoveCoin">
+<div className=" text-center w-full  md:w-[140px] w-[162px] h-[162px] 
+rounded-[24px] p-[32px]   bg-neutral-800  ">
+<img src="Telegram.png"
+ className="sm:w-[38px] sm:h-[40px] w-[48px] h-[50px] mx-auto rounded-xl  "/>
+<h2 className=" f-f-sm text-white text-lg mt-5 ">Telegram</h2>
+</div></a></div>
+<div className=" col-span-6   ">
+<a href="https://twitter.com/PuppyLoveCoin">
+<div className=" text-center w-full
+ md:w-[140px] w-[162px] h-[162px] rounded-[24px] p-[32px]
+ bg-neutral-800 ">
+<img src="Twitter.png" class=" w-[48px] h-[48px] mx-auto "/>
+<h2 className=" f-f-sm text-white text-lg mt-5 ">Twitter</h2></div></a></div>
+</div>
+</div>
+<div className="mt-[10px] col-span-12 md:col-span-6  md:hidden   ">
+<img src="community.jpg" 
+className=" w-full h-[365px] object-cover rounded-[24px] "/>
+</div>
+</div>
 
 
 
--- app/page.js
+++ app/page.js
@@ -1,21 +1,13 @@
-<<<<<<< HEAD
-
-import Image from "next/image";
-import Faq from "./Faqs/page";
+import Faq from "./Faqs/faqs";
 import Team from './Team/page'
 import Community from './Community/page'
 import Footer from'./Footer/page'
-export default function Home() {
-  return (
-    <main className="">
-    <Footer/>
-     
-=======
 import Image from "next/image";
 import Navbar from "./navbar/page";
 import Header  from "./header/page";
 import Feature from "./features/page";
 import Tokennomics from "./Tokennomics/Tokennomics";
+import Faqs from "./Faqs/faqs"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -23,7 +15,12 @@ export default function Home() {
       <Header/>
       <Feature/>
       <Tokennomics/>
->>>>>>> c7eb1888d36a1046df867712941bc3e84293462a
+      <Faqs/>
+      <Team/>
+      <Community/>
+      <Footer/>
+   
+
     </main>
   );
 }
